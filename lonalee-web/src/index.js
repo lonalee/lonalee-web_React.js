a359d188c7ab6540a234fe9e91ac922d59701e7a
lonalee2018 <lonalee2018@gmail.com>
2018-10-17 00:04:19 +0900

    Get Me Contents-xhr instance made

--- lonalee-web/src/index.js
+++ lonalee-web/src/index.js
@@ -22,7 +22,8 @@ import './index.css';
 // }
 
 function Square(props) {
-  // console.log(props);
+  console.log('in Square',props);
+
   return (
     <button className="square" onClick={props.onClick}>
     { props.value }
@@ -33,34 +34,39 @@ function Square(props) {
 class Board extends React.Component {
   // constructor(props) {
   //   super(props);
-  //   this.state = {    // 상태 관리에 필요한 객체 (state 자체는 property) -- 9개의 인덱스를 갖는 배열(O,X,null 관리), 순서를 보장하기 위한 boolean value를 갖는 프로퍼티
+  //   this.state = {    // 상태 관리에 필요한 객체 (state 자체는 property) -- 9개의 인덱스를 갖는 배열(O,X,null 관리), turn을 보장하기 위한 boolean value를 갖는 프로퍼티
   //     squares: Array(9).fill(null),   //  handleClick 메소드에서 squaresV 배열이 X, O 순서를 저장해서 setState로 업데이트 해준다
   //     xIsNext: true
   //   };
   // }
 
   renderSquare(i) {     // renderSquare가 호출될 때 전달된 인수 0~8이 value 속성으로 저장, 그리고 handleClick 메소드로 인자를 전달
+    console.log('works?', i);
     return <Square value={this.props.squares[i]}
     onClick={() => this.props.onClick(i)} />;
   }
+  // 위 코드로 Square 클래스에 value, onClick 프로퍼티가 생성되었고 Square 클래스에서는 props라는 객체로 접근한다.
 
   render() {
     return (
       <div>
         <div className="board-row">
-          {this.renderSquare(0)}
+          {/* {this.renderSquare(0)}
           {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(2)} */}
+          {this.props.squares.forEach((square, i) => {
+            this.renderSquare(i);
+          })}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
+          {/* {this.renderSquare(3)}
           {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare(5)} */}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
+          {/* {this.renderSquare(6)}
           {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSquare(8)} */}
         </div>
       </div>
     );
@@ -76,7 +82,7 @@ class Game extends React.Component {
       }],
       xIsNext: true,
       stepNumber: 0,
-      coordinate: [{col: 0, row: 0}]
+      coordinate: [{col: 0, row: 0}],
     };
   }
 
@@ -86,10 +92,10 @@ class Game extends React.Component {
     // const current = history[history.length - 1];
     const current = history[this.state.stepNumber]
     // current ------> 매 turn마다 생성되는 배열, 여기에 현재까지 마크된 값이 들어있음, length - 1을 해야 현재 index를 참조할 수 있다
+
     const winner = calculateWinner(current.squares);
     const coordi = this.state.coordinate;
     // console.log('current', current);
-    // console.log('history', history);
     // console.log('history, coordi', history, coordi);
 
     const moves = history.map((step, move) => {       // step, move(jump할 곳을 나타내는 인자),_______moves에는 새로운 배열
@@ -98,14 +104,18 @@ class Game extends React.Component {
       `Go to move # ${move} Column ${coordi[move].col} Row ${coordi[move].row}` :
       'Go to game start';
       // const afterJumpto = 'active';
+      let className = 'test';
 
       return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(step, move)}>{desc}</button>
+        <li key={move} >
+          <button className={className} onClick={() => this.jumpTo(move, className)}>{desc}</button>
         </li>
       );
     });
-    console.log('moves',moves);
+
+    // console.log('moves',moves);
+    // const findButton = moves.map(() => {
+    // })
 
     let status;
     if (winner) {
@@ -127,24 +137,17 @@ class Game extends React.Component {
         </div>
       </div>
     );
-  }
-  // --------------------- jumpTo --------------------------
-  jumpTo(step, move) {
 
+  }
+  jumpTo(move, name) {
     this.setState({
       stepNumber: move,
       xIsNext: (move % 2) === 0
     });
-    console.log('array',step);  // clicked history item
-    console.log('which one',move);  //
-
-    // for(let i = 0; i < 9; i++){
-    //   if(step ===)
-    // }
-  }
-
+  };
     // ------------------- Board --------------> Game Component ---------------------
     handleClick(i) {      // 클릭된 square의 value를 인자로 받음
+    console.log('square clicked', i);
       const history = this.state.history.slice(0, this.state.stepNumber + 1);
       // console.log('stepNumber', this.state.stepNumber);
       // console.log('history in handleClick',history); // 메소드 내의 history는
@@ -166,7 +169,7 @@ class Game extends React.Component {
         let row = Math.floor(i / 3);    // row 구하기
         let col = i % 3;    // col 구하기
         coordinate = c.concat({col: col, row: row});
-        console.log('coordinate', coordinate);
+        // console.log('coordinate', coordinate);
       }
 
       if (i < 3) {  //  row: 0 일 경우
@@ -192,7 +195,8 @@ class Game extends React.Component {
     };
 
 }
-// ************** function calculateWinner ********************
+
+// ************** function calculateWinner (functional Component)********************
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -225,7 +229,7 @@ class Mytrial extends React.Component {
     };
     this.handleChange = this.handleChange.bind(this); // ???
     this.handleSubmit = this.handleSubmit.bind(this); // ???
-
+    this.getContents = this.getContents.bind(this);
   }
   render() {
     return (
@@ -237,6 +241,8 @@ class Mytrial extends React.Component {
           <button>Let me know</button>
         </form>
         <LessonList list={this.state.list} />
+        <Getcontents onClick={this.getContents
+        } />
       </div>
     );
   };
@@ -244,10 +250,9 @@ class Mytrial extends React.Component {
     this.setState({ content: e.target.value});
   }
   handleSubmit(e) {
-    e.preventDefault();           // 이 구문이 없으면 자동으로 전체 view를 re-rendering 하였다
+    e.preventDefault();
+    // 이 구문이 없으면 자동으로 전체 view를 re-rendering 하였다
     // 이유는? 기본적으로 form에서 onSubmit()을 통해 submit 하면 이벤트 완료 후 refresh, SPA에서는 지양해야할 부분. 그래서 event.preventDefault() 를 활용하여 추가로 이벤트를 전파하지 않고 취소 가능 -> 다시 말해 submit 되지만 re-rendering은 취소시킴
-
-
     const item = {
       content: this.state.content,
       id: Date.now()
@@ -258,6 +263,9 @@ class Mytrial extends React.Component {
       content: ''
     }));
   };
+  getContents() {
+    console.log('clicked');
+  }
 }
 
 class LessonList extends React.Component {
@@ -272,7 +280,32 @@ class LessonList extends React.Component {
   };
 };
 
-// ========================================
+class Getcontents extends React.Component {
+  getContents() {
+    console.log('clicked');
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '/contents');
+    xhr.send();
+
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) console.log(xhr.response);
+        else console.log(xhr.status);
+      }
+    }
+  }
+  render () {
+    return (
+      <div>
+      <button onClick={() => {
+        this.getContents();
+      }}>Get Me contents</button>
+      </div>
+    )
+  }
+}
+
+// ========================================ReactDOM.ender()=====================
 
 ReactDOM.render(
   <Game />,
